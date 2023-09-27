ec44f84e3e716e8a11afafd95232d875484c3c26
Jay Jitendrakumar Mulani <jy850028@dal.ca>
2023-09-26 23:26:28 -0300

    Project Kubernetes

--- K8s/k8s/container1/controller/index.js
+++
@@ -1,7 +0,0 @@
-const controller = require("./controller")
-const router = require("express").Router()
-
-router.post("/calculate", controller.calculate);
-router.post("/store-file",controller.storefile)
-
-module.exports = router
--- K8s/k8s/container2/controller/index.js
+++
@@ -1,6 +0,0 @@
-const controller = require("./controller")
-const router = require("express").Router()
-
-router.post("/parse", controller.parse);
-
-module.exports = router
